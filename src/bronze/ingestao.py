a8ecf919fda2b357034ab8f682366493807999e0
heitorfelix <heitorfelix>
2024-09-28 12:51:34 +0000

    cdc, workflow, parameters and schema

--- src/bronze/ingestao.py
+++ src/bronze/ingestao.py
@@ -1,60 +1,84 @@
 # Databricks notebook source
+# DBTITLE 1,Imports
 import delta
+import json
+from pyspark.sql import types
+from utils import table_exists, import_schema
+
+
+
 
 # COMMAND ----------
 
-# DBTITLE 1,IMPORTS
+# DBTITLE 1,Setup
 schema_name = 'olist'
-table_name = 'orders'
+catalog = 'bronze'
 
-df_full = spark.read.format("parquet").load(f"/Volumes/raw/{schema_name}/full/{table_name}/")
 
-(df_full.coalesce(1)
-        .write
-        .format("delta")
-        .mode("overwrite")
-        .saveAsTable("bronze.olist.orders"))
+# table_name = 'orders'
+# id_column = 'order_id'
+# timestamp_column = 'modified_at'
+
+table_name = dbutils.widgets.get('table_name')
+id_column =  dbutils.widgets.get('id_column')
+timestamp_column =  dbutils.widgets.get('timestamp_column')
+
+schema = import_schema(table_name=table_name)
+
+
 
 # COMMAND ----------
 
+# DBTITLE 1,Full Load
+if table_exists(spark, catalog, schema_name, table_name):
+        print(f'Tabela {table_name} já existe. Pulando carga Full.')
+
+else:
+        print(f'Tabela  {table_name} não existe. Fazendo carga Full...')
+
+        df_full = (spark.read
+                        .format("parquet")
+                        .schema(schema)
+                        .load(f"/Volumes/raw/{schema_name}/full/{table_name}/"))
+        
+        
+        (df_full.coalesce(1)
+                .write
+                .format("delta")
+                .mode("overwrite")
+                .saveAsTable(f"bronze.{schema_name}.{table_name}"))
+
+# COMMAND ----------
+
+# DBTITLE 1,CDC
+
+
+# Creating temp view to load cdc
 (spark.read
       .format("parquet")
+      .schema(schema)
       .load(f"/Volumes/raw/{schema_name}/cdc/{table_name}/")
       .createOrReplaceTempView(table_name))
 
-# COMMAND ----------
-
-bronze = delta.DeltaTable
-
-# COMMAND ----------
-
-query = """ 
-    SELECT * 
-    FROM orders
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY order_id ORDER BY (SELECT 1) DESC) = 1
+# Get last value
+query = f""" 
+SELECT * 
+FROM {table_name}
+QUALIFY ROW_NUMBER() OVER (PARTITION BY {id_column} ORDER BY {timestamp_column} DESC) = 1
 """
 
-df_cdc_unique = spark.sql(query)
-df_cdc_unique.display()
+df_cdc_last = spark.sql(query)
 
-# COMMAND ----------
-
-bronze = delta.DeltaTable.forName(spark, f"bronze.olist.{table_name}")
-
-# COMMAND ----------
 
 #upsert
+bronze = delta.DeltaTable.forName(spark, f"bronze.{schema_name}.{table_name}")
 
 (bronze.alias("b")
-       .merge(df_cdc_unique.alias("d"),
+       .merge(df_cdc_last.alias("d"),
        "b.order_id = d.order_id")
        .whenMatchedDelete(condition= "d.Op = 'D'")
        .whenMatchedUpdateAll(condition= "d.Op = 'U'")
        .whenNotMatchedInsertAll(condition= "d.Op = 'I' or d.Op = 'U'")
        .execute()
 )
-
-
-# COMMAND ----------
-
 
