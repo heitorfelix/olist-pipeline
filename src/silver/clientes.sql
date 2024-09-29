7dbf6715c6b5147a4e604eb67bdaaa60c983feef
heitorfelix <heitorfelix>
2024-09-29 20:15:39 +0000

    introduce customers and products tables

---
+++ src/silver/clientes.sql
@@ -0,0 +1,7 @@
+SELECT 
+  customer_id                          AS idCliente,
+  customer_unique_id                   AS idClienteUnico,
+  customer_zip_code_prefix              AS prefixCepCliente,
+  customer_city                         AS descCidadeCliente,
+  customer_state                        AS descEstadoCliente
+FROM bronze.olist.customers;
