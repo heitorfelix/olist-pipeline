5db7e9e0c2da84b139b82f56ac7b72e7449a4f49
heitorfelix <heitorfelix>
2024-09-29 10:40:52 +0000

    added Ingestor classes and streaming CDC

--- src/bronze/ingestao.py
+++ src/bronze/ingestao.py
@@ -1,4 +1,8 @@
 # Databricks notebook source
+dbutils.library.restartPython()
+
+# COMMAND ----------
+
 # DBTITLE 1,Imports
 import delta
 import json
@@ -9,8 +13,7 @@ sys.path.insert(0, "../lib/")
 
 
 from utils import table_exists, import_schema
-
-
+from ingestors import Ingestor, IngestorCDC
 
 
 # COMMAND ----------
@@ -19,71 +22,49 @@ from utils import table_exists, import_schema
 schema_name = 'olist'
 catalog = 'bronze'
 
+table_name = dbutils.widgets.get('table_name')
+id_column =  dbutils.widgets.get('id_column')
+timestamp_column =  dbutils.widgets.get('timestamp_column')
 
 # table_name = 'orders'
 # id_column = 'order_id'
 # timestamp_column = 'modified_at'
 
-table_name = dbutils.widgets.get('table_name')
-id_column =  dbutils.widgets.get('id_column')
-timestamp_column =  dbutils.widgets.get('timestamp_column')
-
+cdc_path = f"/Volumes/raw/{schema_name}/cdc/{table_name}/"
+full_load_path = f"/Volumes/raw/{schema_name}/full/{table_name}/"
+checkpoint_location = f"/Volumes/raw/{schema_name}/checkpoint/{table_name}/"
 schema = import_schema(table_name=table_name)
 
 
 
 # COMMAND ----------
 
-# DBTITLE 1,Full Load
+# DBTITLE 1,Full load class
 if table_exists(spark, catalog, schema_name, table_name):
         print(f'Tabela {table_name} já existe. Pulando carga Full.')
 
 else:
+        
+        ingestor = Ingestor(spark
+                               , catalog = catalog
+                               , schema_name = schema_name
+                               , table_name = table_name
+                               , data_format = 'parquet') 
+        
+        dbutils.fs.rm(ingestor.checkpoint_location, True)
         print(f'Tabela  {table_name} não existe. Fazendo carga Full...')
+        ingestor.execute(path=full_load_path)
 
-        df_full = (spark.read
-                        .format("parquet")
-                        .schema(schema)
-                        .load(f"/Volumes/raw/{schema_name}/full/{table_name}/"))
-        
-        
-        (df_full.coalesce(1)
-                .write
-                .format("delta")
-                .mode("overwrite")
-                .saveAsTable(f"bronze.{schema_name}.{table_name}"))
 
 # COMMAND ----------
 
-# DBTITLE 1,CDC
+# DBTITLE 1,CDC class
+ingestor = IngestorCDC(spark
+                        , catalog = catalog
+                        , schema_name = schema_name
+                        , table_name = table_name
+                        , data_format = 'parquet'
+                        , id_field = id_column
+                        , timestamp_field = timestamp_column) 
 
-
-# Creating temp view to load cdc
-(spark.read
-      .format("parquet")
-      .schema(schema)
-      .load(f"/Volumes/raw/{schema_name}/cdc/{table_name}/")
-      .createOrReplaceTempView(table_name))
-
-# Get last value
-query = f""" 
-SELECT * 
-FROM {table_name}
-QUALIFY ROW_NUMBER() OVER (PARTITION BY {id_column} ORDER BY {timestamp_column} DESC) = 1
-"""
-
-df_cdc_last = spark.sql(query)
-
-
-#upsert
-bronze = delta.DeltaTable.forName(spark, f"bronze.{schema_name}.{table_name}")
-
-(bronze.alias("b")
-       .merge(df_cdc_last.alias("d"),
-       "b.order_id = d.order_id")
-       .whenMatchedDelete(condition= "d.Op = 'D'")
-       .whenMatchedUpdateAll(condition= "d.Op = 'U'")
-       .whenNotMatchedInsertAll(condition= "d.Op = 'I' or d.Op = 'U'")
-       .execute()
-)
-
+ingestor.execute(cdc_path)
---
+++ src/lib/ingestors.py
@@ -0,0 +1,83 @@
+import delta
+from utils import import_schema
+
+
+class Ingestor:
+
+    def __init__(self, spark, catalog, schema_name, table_name, data_format):
+        self.spark = spark 
+        self.catalog = catalog
+        self.schema_name = schema_name
+        self.table_name = table_name
+        self.format = data_format
+        self.set_schema()
+        self.checkpoint_location = f"/Volumes/raw/{self.schema_name}/cdc/{self.table_name}_checkpoint/"
+
+    def set_schema(self):
+        self.data_schema = import_schema(self.table_name)
+
+    def load(self, path):
+        df = (self.spark
+                  .read
+                  .format(self.format)
+                  .schema(self.data_schema)
+                  .load(path))
+        return df
+        
+    def save(self, df):
+        (df.write
+           .format("delta")
+           .mode("overwrite")
+           .saveAsTable(f"{self.catalog}.{self.schema_name}.{self.table_name}"))
+        return True
+        
+    def execute(self, path):
+        df = self.load(path)
+        return self.save(df)
+    
+class IngestorCDC(Ingestor):
+
+    def __init__(self, spark, catalog, schema_name, table_name, data_format, id_field, timestamp_field):
+        super().__init__(spark, catalog, schema_name, table_name, data_format)
+        self.id_field = id_field
+        self.timestamp_field = timestamp_field
+        self.set_deltatable()
+
+    def set_deltatable(self):
+        table_name = f"{self.catalog}.{self.schema_name}.{self.table_name}"
+        self.deltatable = delta.DeltaTable.forName(self.spark, table_name)
+
+    def upsert(self, df):
+        df.createOrReplaceGlobalTempView(f"view_{self.table_name}")
+        query = f'''
+            SELECT *
+            FROM global_temp.view_{self.table_name}
+            QUALIFY ROW_NUMBER() OVER (PARTITION BY {self.id_field} ORDER BY {self.timestamp_field} DESC) = 1
+        '''
+
+        df_cdc = self.spark.sql(query)
+
+        (self.deltatable
+             .alias("b")
+             .merge(df_cdc.alias("d"), f"b.{self.id_field} = d.{self.id_field}") 
+             .whenMatchedDelete(condition = "d.OP = 'D'")
+             .whenMatchedUpdateAll(condition = "d.OP = 'U'")
+             .whenNotMatchedInsertAll(condition = "d.OP = 'I' OR d.OP = 'U'")
+             .execute())
+
+    def load(self, path):
+        df = (self.spark
+                  .readStream
+                  .format("cloudFiles")
+                  .option("cloudFiles.format", self.format)
+                  .schema(self.data_schema)
+                  .load(path))
+        return df
+    
+    def save(self, df):
+        stream = (df.writeStream
+                   .option("checkpointLocation", self.checkpoint_location)
+                   .foreachBatch(lambda df, batchID: self.upsert(df))
+                   .trigger(availableNow=True))
+        return stream.start()
+    
